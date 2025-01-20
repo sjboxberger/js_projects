30aa89a8c524fccaadf6edf4937aa97f05f10d8b
Seth <sjboxberger@gmail.com>
2025-01-19 19:04:16 -0600

    Allowing helper files to work

--- cc_projects/test1/script.js
+++ cc_projects/test1/script.js
@@ -1,118 +1,118 @@
 
-const books = [
-    {
-      title: "The City We Became",
-      author: "N. K. Jemisin",
-      tags: ["fantasy", "fiction", "afrofutursim", "science fiction", "sci-fi"]
-    },
-    {
-      title: "The Catcher in the Rye",
-      author: "J. D. Salinger",
-      tags: ["fiction", "young adult", "YA", "realism", "coming of age", "classic"]
-    },
-    {
-      title: "The Hundred Thousand Kingdoms",
-      author: "N. K. Jemisin",
-      tags: ["fantasy", "fiction", "adventure", "series"]
-    },
-    {
-      title: "Sapiens: A Brief History of Humankind",
-      author: "Yuval Noah Harari",
-      tags: ["nonfiction", "history", "anthropology", "science", "sociology"]
-    },
-    {
-      title: "Behave: The Biology of Humans at Our Best and Worst",
-      author: "Robert M. Sapolsky",
-      tags: ["nonfiction", "anthropology", "science", "sociology", "biology"]
-    },
-    {
-      title: "The Parable of the Talents",
-      author: "Octavia Butler",
-      tags: ["fiction", "dystopian", "science fiction"]
-    },
-    {
-      title: "1984",
-      author: "George Orwell",
-      tags: ["fiction", "dystopian", "science fiction", "classics", "adult"]
-    },
-    {
-      title: "Remarkably Bright Creatures",
-      author: "Shelby Van Pelt",
-      tags: ["fiction", "mystery", "magical realism"]
-    },
-    {
-      title: "Crying in H Mart",
-      author: "Michelle Zauner",
-      tags: ["memoir", "nonfiction", "autobiography"]
-    },
-    {
-      title: "Wild: From Lost to Found on the Pacific Crest Trail",
-      author: "Cheryl Strayed",
-      tags: ["nonfiction", "memoir", "adventure", "travel"]
-    }
-  ]
+// const books = [
+//     {
+//       title: "The City We Became",
+//       author: "N. K. Jemisin",
+//       tags: ["fantasy", "fiction", "afrofutursim", "science fiction", "sci-fi"]
+//     },
+//     {
+//       title: "The Catcher in the Rye",
+//       author: "J. D. Salinger",
+//       tags: ["fiction", "young adult", "YA", "realism", "coming of age", "classic"]
+//     },
+//     {
+//       title: "The Hundred Thousand Kingdoms",
+//       author: "N. K. Jemisin",
+//       tags: ["fantasy", "fiction", "adventure", "series"]
+//     },
+//     {
+//       title: "Sapiens: A Brief History of Humankind",
+//       author: "Yuval Noah Harari",
+//       tags: ["nonfiction", "history", "anthropology", "science", "sociology"]
+//     },
+//     {
+//       title: "Behave: The Biology of Humans at Our Best and Worst",
+//       author: "Robert M. Sapolsky",
+//       tags: ["nonfiction", "anthropology", "science", "sociology", "biology"]
+//     },
+//     {
+//       title: "The Parable of the Talents",
+//       author: "Octavia Butler",
+//       tags: ["fiction", "dystopian", "science fiction"]
+//     },
+//     {
+//       title: "1984",
+//       author: "George Orwell",
+//       tags: ["fiction", "dystopian", "science fiction", "classics", "adult"]
+//     },
+//     {
+//       title: "Remarkably Bright Creatures",
+//       author: "Shelby Van Pelt",
+//       tags: ["fiction", "mystery", "magical realism"]
+//     },
+//     {
+//       title: "Crying in H Mart",
+//       author: "Michelle Zauner",
+//       tags: ["memoir", "nonfiction", "autobiography"]
+//     },
+//     {
+//       title: "Wild: From Lost to Found on the Pacific Crest Trail",
+//       author: "Cheryl Strayed",
+//       tags: ["nonfiction", "memoir", "adventure", "travel"]
+//     }
+//   ]
 
-  /////////////////////////////////////////////////////////////////////////////////////////////////
-  const flattenObjectValuesIntoArray = (arrOfObjs) => {
-    let flattenedObj;
-    const flattenedObjsArr = [];
-    for (let obj = 0; obj < arrOfObjs.length; obj++) {
-      const objValues = Object.values(arrOfObjs[obj]);
-      flattenedObj = [...objValues.flat()]
-      flattenedObjsArr.push(flattenedObj)
-    }
-    return flattenedObjsArr;
-};
+//   /////////////////////////////////////////////////////////////////////////////////////////////////
+//   const flattenObjectValuesIntoArray = (arrOfObjs) => {
+//     let flattenedObj;
+//     const flattenedObjsArr = [];
+//     for (let obj = 0; obj < arrOfObjs.length; obj++) {
+//       const objValues = Object.values(arrOfObjs[obj]);
+//       flattenedObj = [...objValues.flat()]
+//       flattenedObjsArr.push(flattenedObj)
+//     }
+//     return flattenedObjsArr;
+// };
 
-const structureBookAsHtml = (book) => {
-/*
-    Book object: book={ title, author, tags(array)}
-*/
-    const bookDiv = document.createElement("div");
-    bookDiv.setAttribute('class', 'bookDiv');
+// const structureBookAsHtml = (book) => {
+// /*
+//     Book object: book={ title, author, tags(array)}
+// */
+//     const bookDiv = document.createElement("div");
+//     bookDiv.setAttribute('class', 'bookDiv');
 
-    const bookTitle = document.createElement("h2");
-    bookTitle.innerHTML = book.title;
-    bookTitle.setAttribute('class', 'bookTitle');
-
-
-    const bookAuthor = document.createElement("h3");
-    bookAuthor.innerHTML = book.author;
+//     const bookTitle = document.createElement("h2");
+//     bookTitle.innerHTML = book.title;
+//     bookTitle.setAttribute('class', 'bookTitle');
 
 
-    const bookTags = document.createElement("p");
-    bookTags.innerHTML = book.tags.join(", ");
+//     const bookAuthor = document.createElement("h3");
+//     bookAuthor.innerHTML = book.author;
 
 
-    bookDiv.append(bookTitle, bookAuthor, bookTags);
-
-    return bookDiv;
-/*
-
-    returns the following div to function caller
-
-    <div class='bookdiv'>                           example:
-        <h2 class='booktitle'>book.title</h2>      1984
-        <h3>book.author</h3>                       George Orwell
-        <p>book.tags</p>                           fiction, dystopian, science fiction, classics, adult
-    </div>
-
-*/
-};
+//     const bookTags = document.createElement("p");
+//     bookTags.innerHTML = book.tags.join(", ");
 
 
-const renderBooksToDom = (elements) => {
-    const bookListContainer = document.querySelector("#bookList");
+//     bookDiv.append(bookTitle, bookAuthor, bookTags);
 
-    //Resets search results area
-    bookListContainer.innerHTML = "";
+//     return bookDiv;
+// /*
+
+//     returns the following div to function caller
+
+//     <div class='bookdiv'>                           example:
+//         <h2 class='booktitle'>book.title</h2>      1984
+//         <h3>book.author</h3>                       George Orwell
+//         <p>book.tags</p>                           fiction, dystopian, science fiction, classics, adult
+//     </div>
+
+// */
+// };
 
 
-    //Appends new divs based on array parameter (should be structured as divs)
-    bookListContainer.append(...elements);
-};
+// const renderBooksToDom = (elements) => {
+//     const bookListContainer = document.querySelector("#bookList");
 
-///////////////////////////////////////////////////////////////////////////////////////////////////
+//     //Resets search results area
+//     bookListContainer.innerHTML = "";
+
+
+//     //Appends new divs based on array parameter (should be structured as divs)
+//     bookListContainer.append(...elements);
+// };
+
+// ///////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 
